c334d4296aa69801453a2c111b48c6efb7086fda
Aaudh Kumar <aaudhkumar01@gmail.com>
2026-03-08 21:48:21 +0530

    bug : same numbers

--- script.js
+++ script.js
@@ -5,6 +5,10 @@ var enter = document.getElementById('enter');
 
 btn.addEventListener('click' , function(){
     let rndmNumber = Number(inpt1.value)  +  Math.random() * Number(inpt2.value - inpt1.value) ; 
+    if(inpt1.value === inpt2.value){
+        alert("Both values are same , Change one of the value ") ; 
+        return ;
+    }
     enter.innerText = Math.round(rndmNumber) ; 
     console.log(rndmNumber) ; 
     console.log(inpt1.value) ; 
