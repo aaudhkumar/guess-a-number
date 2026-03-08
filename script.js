ff008c07822e508c50abebab0beb7ec73a4a310e
Aaudh Kumar <aaudhkumar01@gmail.com>
2026-03-08 17:24:10 +0530

    random number generator

---
+++ script.js
@@ -0,0 +1,13 @@
+var inpt1 = document.getElementById('inpt1') ; 
+var inpt2 = document.getElementById('inpt2');
+var btn = document.getElementById('btn');
+var enter = document.getElementById('enter');
+
+btn.addEventListener('click' , function(){
+    let rndmNumber = Number(inpt1.value)  +  Math.random() * Number(inpt2.value - inpt1.value) ; 
+    enter.innerText = Math.round(rndmNumber) ; 
+    console.log(rndmNumber) ; 
+    console.log(inpt1.value) ; 
+    console.log(inpt2.value) ; 
+
+})
